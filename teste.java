d798808e93d172fe4c7d8cefd7d6c207809f47f3
Vitoria <vssimoes24@gmail.com>
2022-05-12 22:21:07 -0300

    testando

---
+++ teste.java
@@ -0,0 +1,13 @@
+package exemplojava;
+public class Pessoa{
+    private String nome;
+    private String cpf;
+
+    public Pessoa(String nome, String cpf){
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+    public void imprimi(){
+        
+    }
+}
